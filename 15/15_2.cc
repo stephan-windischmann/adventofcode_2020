3512fb08f90d26f3d8c65bc42156457b9ff4056d
Stephan Windischmann <stephan.windischmann@swisscom.com>
2020-12-17 13:42:08 +0100

    Improve performance a bit and complete day 15 part 2.

---
+++ 15/15_2.cc
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <utility>
+
+#define INPUT  "input"
+
+void loadInput(std::map<int, std::pair<int, int> > &input, \
+        int &turn, int &lastNum) {
+    std::ifstream file;
+    file.open(INPUT);
+
+    std::string in;
+    turn = 1;
+    while (std::getline(file, in, ',')) {
+        int cur = std::stoi(in);
+        input[cur] = std::pair<int, int>{turn, turn};
+        lastNum = cur;
+        ++turn;
+    }
+
+    file.close();
+}
+
+int playGame(int finalTurn, int &turn, int &lastNum, \
+        std::map<int, std::pair<int, int> > &input) {
+    while (turn <= finalTurn) {
+        int newNum = input[lastNum].second - input[lastNum].first;
+
+        if (input.find(newNum) == input.end()) {
+            input[newNum] = std::pair<int, int>{turn, turn};
+        } else {
+            input[newNum].first = input[newNum].second;
+            input[newNum].second = turn;
+        }
+        
+        lastNum = newNum;
+        ++turn;
+    }
+
+    return lastNum;
+}
+
+int main() {
+    std::map<int, std::pair<int, int> > input;
+    int turn;
+    int lastNum;
+    
+    loadInput(input, turn, lastNum);
+
+    int res = playGame(30000000, turn, lastNum, input);
+
+    std::cout << "Result: " << res << std::endl;
+
+    return 0;
+}
+
